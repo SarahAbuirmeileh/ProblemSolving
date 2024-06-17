126d8204302bd6ea99e9459dbfbed71ead25cedf
Sarah Abu irmeileh <211027@ppu.edu.ps>
2024-06-17 19:37:30 +0300

    405A

---
+++ 405A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n" 
+#define int long long
+#define tee { ios :: sync_with_stdio(false); cin.tie(0); cout.tie(0); }
+
+void solve();
+int32_t main() 
+{
+    tee
+    int t=1;
+    // cin >> t;
+    while (t--) {
+       solve();
+    }
+    return 0;
+}
+
+void solve(){
+
+    int n; cin >> n;
+    vector<int>v(n);
+
+    for (int i=0; i<n; i++) {
+        cin >> v[i]; 
+    }
+
+    sort(v.begin(),v.end());
+
+    for (int i=0; i<n; i++) {
+        cout <<  v[i] << " "; 
+    }
+}
