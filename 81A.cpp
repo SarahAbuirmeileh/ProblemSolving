e53f9d6c76ff74fd940f170816dad38854aed166
Sarah Abu irmeileh <211027@ppu.edu.ps>
2024-06-18 12:23:19 +0300

    81A

---
+++ 81A.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n" 
+#define int long long
+#define tee { ios :: sync_with_stdio(false); cin.tie(0); cout.tie(0); }
+
+void solve();
+int32_t main() {
+    tee
+    int t=1;
+    // cin >> t;
+    while (t--) {
+       solve();
+    }
+    return 0;
+}
+
+void solve(){
+
+    string str; cin >> str; 
+    stack<char>s;
+
+    for (char ss : str){
+        if(s.empty()){
+            s.push(ss);
+        }else if(s.top()==ss){
+            s.pop();
+        }else{
+            s.push(ss);
+        }
+    }
+
+    string ans = "";
+    while(!s.empty()){
+        ans = s.top()+ans;
+        s.pop();
+    }
+    cout << ans<<endl;
+}
