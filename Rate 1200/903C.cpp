0ce59ba5e23e6580d344b1ed1427470245099788
Sarah Abu irmeileh <211027@ppu.edu.ps>
2024-06-18 20:43:45 +0300

    903C

---
+++ Rate 1200/903C.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n" 
+#define int long long
+#define tee { ios :: sync_with_stdio(false); cin.tie(0); cout.tie(0); }
+
+void solve();
+int32_t main() {
+    tee
+    int t=1;
+    // cin >> t;
+    while (t--) {
+       solve();
+    }
+    return 0;
+}
+
+void solve(){
+
+    int n, x;
+    cin >> n;
+    map<int, int>m;
+
+    for (int i=0; i<n; i++) {
+        cin >> x;
+        m[x]++;
+    }
+
+    int ans = 0;
+    for (auto it : m) {
+        ans = max (ans, it.second);
+    }
+    cout << ans ;
+}
