7bba5cf7a8da8ad2cd960233469c36b1fcd68845
Sarah Abu irmeileh <211027@ppu.edu.ps>
2024-06-23 15:16:28 +0300

    1526C2

---
+++ Rate 1600/1526C2.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n" 
+#define int long long
+#define tee { ios :: sync_with_stdio(false); cin.tie(0); cout.tie(0); }
+#define not_less_than(v, x) lower_bound(v.begin(), v. end(), x)                   // x >=
+#define not_more_than(v, x) lower_bound(v.rbegin(), v.rend(), x, greater<int>())  // x <=
+#define more_than(v, x) upper_bound (v.begin(), v.end(), x)                       // x <
+#define less_than(v, x)upper_bound (v. rbegin(), v.rend(), x, greater<int>())     // x >
+
+void solve();
+int32_t main() {
+    tee
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+       solve();
+    }
+    return 0;
+}
+
+void solve(){
+
+    int n; cin >> n;
+    vector<int>v(n);
+    priority_queue<int, vector<int>, greater<int>>pq;
+
+    int sum = 0, ans = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> v[i]; 
+        pq.push(v[i]);
+        sum += v[i];
+        ans++;
+        if(sum < 0){
+            sum -= pq.top();
+            pq.pop();
+            ans--;
+        }
+    }
+    cout << ans << endl;
+}
