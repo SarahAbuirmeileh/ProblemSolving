972d98891757ab3a110e646d4822d6e2342640d6
Sarah Abu irmeileh <211027@ppu.edu.ps>
2024-07-09 13:12:38 +0300

    242C

--- Rate 1800/242C.cpp
+++ Rate 1800/242C.cpp
@@ -1,62 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define endl "\n" 
+#define endl "\n"
 #define int long long
 #define tee { ios :: sync_with_stdio(false); cin.tie(0); cout.tie(0); }
 
+void solve();
+int32_t main() {
+    tee;
+    int t = 1;
+    // cin >> t;
+    for(int i = 1; i <= t; i++){
+        solve();
+    }
+    return 0;
+}
 
-
-int BFS(int x0, int y0, int x1, int y1, const set<pair<int, int>>& cells) {
-    vector<pair<int, int>> dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, 
-                                  {1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
-    
-    map<pair<int, int>, int> dis;
+int BFS(int x0, int y0, int x1, int y1, set<pair<int ,int>>&cells) {
     queue<pair<int, int>> q;
-
     q.push({x0, y0});
-    dis[{x0, y0}] = 0;
+
+    map<pair<int, int>, int>m;
+    m[{x0, y0}]=0;
+
+    vector<pair<int, int>> dis = { {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1} };
 
     while (!q.empty()) {
         pair<int, int> t = q.front(); q.pop();
         int x = t.first, y = t.second;
 
         if (x == x1 && y == y1) {
-            return dis[{x, y}];
+            return m[{x, y}];
         }
-
-        for (auto& d : dir) {
+        
+        for (auto d : dis) {
             int nx = x + d.first, ny = y + d.second;
-            if (cells.count({nx, ny}) && !dis.count({nx, ny})) {
-                dis[{nx, ny}] = dis[{x, y}] + 1;
+            
+            if (!m.count({nx, ny}) && cells.count({nx, ny})) {
                 q.push({nx, ny});
+                m[{nx, ny}] = m[{x, y}]+1;
             }
         }
     }
-
-    return -1;
+    return -1; 
 }
 
 void solve() {
-    int x0, y0, x1, y1;
-    cin >> x0 >> y0 >> x1 >> y1;
+    int x0, y0, x1, y1; cin >> x0 >> y0 >> x1 >> y1;
 
     int n; cin >> n;
-    set<pair<int, int>> cells;
-    for (int i = 0; i < n; ++i) {
-        int ri, ai, bi;
-        cin >> ri >> ai >> bi;
-        for (int j = ai; j <= bi; ++j) {
-            cells.insert({ri, j});
+    int r, a, b;
+    set<pair<int ,int>>cells;
+    for(int i=0; i<n; i++){
+        cin >> r >> a >> b;
+        for(int j = a; j<= b; j++){
+            cells.insert({r, j});
         }
     }
-
-    int result = BFS(x0, y0, x1, y1, cells);
-    cout << result << endl;
+    cout << BFS(x0, y0, x1, y1, cells) << endl;
 }
 
-int32_t main() {
-    tee
-    solve();
-    return 0;
-}
--- prob.cpp
+++ prob.cpp
@@ -5,47 +5,58 @@ using namespace std;
 #define int long long
 #define tee { ios :: sync_with_stdio(false); cin.tie(0); cout.tie(0); }
 
-int BFS(int sx, int sy, int tx, int ty, vector<vector<bool>>& vis) {
+void solve();
+int32_t main() {
+    tee;
+    int t = 1;
+    // cin >> t;
+    for(int i = 1; i <= t; i++){
+        solve();
+    }
+    return 0;
+}
+
+int BFS(int x0, int y0, int x1, int y1, set<pair<int ,int>>&cells) {
     queue<pair<int, int>> q;
-    q.push({sx, sy});
-    vis[sx][sy] = true;
+    q.push({x0, y0});
 
-    vector<vector<int>> cost(8, vector<int>(8, 0));
+    map<pair<int, int>, int>m;
+    m[{x0, y0}]=0;
 
-    vector<pair<int, int>> dis = { {1, 2}, {-1, 2}, {1, -2}, {-1, -2}, 
-                                   {2, 1}, {-2, 1}, {2, -1}, {-2, -1} };
-    
+    vector<pair<int, int>> dis = { {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1} };
+
     while (!q.empty()) {
         pair<int, int> t = q.front(); q.pop();
         int x = t.first, y = t.second;
 
-        if (x == tx && y == ty) {
-            return cost[x][y];
+        if (x == x1 && y == y1) {
+            return m[{x, y}];
         }
         
         for (auto d : dis) {
             int nx = x + d.first, ny = y + d.second;
             
-            if (nx >= 0 && nx <= 7 && ny >= 0 && ny <= 7 && !vis[nx][ny]) {
+            if (!m.count({nx, ny}) && cells.count({nx, ny})) {
                 q.push({nx, ny});
-                vis[nx][ny] = true;
-                cost[nx][ny] = cost[x][y] + 1;
+                m[{nx, ny}] = m[{x, y}]+1;
             }
         }
     }
     return -1; 
 }
 
-int solve(string s, string t) {
-    vector<vector<bool>> vis(8, vector<bool>(8, false));
-    return BFS(s[0] - 'a', s[1] - '1', t[0] - 'a', t[1] - '1', vis);
+void solve() {
+    int x0, y0, x1, y1; cin >> x0 >> y0 >> x1 >> y1;
+
+    int n; cin >> n;
+    int r, a, b;
+    set<pair<int ,int>>cells;
+    for(int i=0; i<n; i++){
+        cin >> r >> a >> b;
+        for(int j = a; j<= b; j++){
+            cells.insert({r, j});
+        }
+    }
+    cout << BFS(x0, y0, x1, y1, cells) << endl;
 }
 
-int32_t main() {
-    tee;
-    string s, t;
-    while (cin >> s >> t) {
-        cout << "To get from " << s << " to " << t << " takes " << solve(s, t) << " knight moves." << endl;
-    }
-    return 0;
-}
