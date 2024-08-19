643269adae0d620c246560b411a872588a87e29f
Sarah Abu irmeileh <211027@ppu.edu.ps>
2024-08-19 11:29:18 +0300

    Leetcode 19

--- Leetcode/Medium/19.cpp
+++ Leetcode/Medium/19.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+
         int size = 0;
-        ListNode* cur = head;
+        ListNode* cur= head;
         while(cur){
-            size++;
             cur = cur->next;
+            size++;
         }
 
         cur = head;
-        for(int i =0; i<size - n; i++){
+        if(size == n){
+            head = head->next;
+            delete cur;
+            return head;
+        }
+
+        for(int i=1; i<size - n; i++){
             cur = cur->next;
         }
-        
-        if (size == n) {
-            ListNode* newHead = head->next;
-            delete head; 
-            return newHead;
-        }
-        
-        cur = head;
-        for (int i = 1; i < size - n; i++) {
-            cur = cur->next;
-        }
-        
-        ListNode* toDelete = cur->next;
+
+        ListNode* del = cur->next;
         cur->next = cur->next->next;
-        
-        delete toDelete;
+        delete del;
         
         return head;
     }
