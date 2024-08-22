a8e7da124dde2e327ede58d06c838ae698e4b420
Sarah Abu irmeileh <211027@ppu.edu.ps>
2024-08-22 11:23:33 +0300

    263

---
+++ CodeForces/Rate 800/263.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n" 
+#define int long long
+#define tee { ios :: sync_with_stdio(false); cin.tie(0); cout.tie(0); }
+#define not_less_than(v, x) lower_bound(v.begin(), v. end(), x)                   // x >=
+#define not_more_than(v, x) lower_bound(v.rbegin(), v.rend(), x, greater<int>())  // x <=
+#define more_than(v, x) upper_bound (v.begin(), v.end(), x)                       // x <
+#define less_than(v, x)upper_bound (v. rbegin(), v.rend(), x, greater<int>())     // x >
+
+const int dx[] = { 0,0,1,-1 };
+const int dy[] = { 1,-1,0,0 };
+
+void solve();
+int32_t main() {
+    tee
+    // #ifndef ONLINE_JUDGE 
+    //     freopen("input.txt", "r", stdin); 
+    //     freopen("output.txt", "w", stdout); 
+    // #endif 
+    int t = 1;
+    // cin >> t;
+    for(int i = 1; i <= t; i++){
+        solve();
+    }
+    
+    return 0;
+}
+
+void solve(){
+    int x, y, c;
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            cin >> c;
+            if(c){
+                x = i, y=j;
+            }
+        }
+    }
+    cout << abs(2-x) + abs(2-y) << endl;
+}
