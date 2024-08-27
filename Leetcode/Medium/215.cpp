8a2377445697931567b566bb736ff20c5ac1f2bb
Sarah Abu irmeileh <211027@ppu.edu.ps>
2024-08-27 19:43:50 +0300

    Leetcode 215

---
+++ Leetcode/Medium/215.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int>pq;
+        for(int n : nums){
+            pq.push(n);
+        }
+        int ans;
+        while(--k){
+           pq.pop();
+        }
+        return pq.top();
+    }
+};
