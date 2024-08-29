41e44fa4d2657bc392d0afacbdeb5c89f8a23d91
Sarah Abu irmeileh <211027@ppu.edu.ps>
2024-08-29 20:41:01 +0300

    Leetcode 1047

---
+++ Leetcode/Easy/1047.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char> st;
+        for (char c : s) {
+            if (!st.empty() && st.top() == c) {
+                st.pop();
+            } else {
+                st.push(c);
+            }
+        }
+        string ans(st.size(), ' '); 
+        for (int i = st.size() - 1; i >= 0; --i) {
+            ans[i] = st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
