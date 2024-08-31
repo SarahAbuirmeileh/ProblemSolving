c5131b496122d7b00566b7b22c554a3c906f29e9
Sarah Abu irmeileh <211027@ppu.edu.ps>
2024-08-31 14:06:09 +0300

    Leetcode 142

---
+++ Leetcode/Medium/142.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast && fast->next){
+            slow = slow->next;
+            fast= fast->next->next;
+            if(fast == slow){
+                ListNode* h = head;
+                while(head != slow){
+                    head = head->next;
+                    slow = slow->next;
+                }
+                return slow;
+            }
+        }
+        return NULL;
+    }
+};
