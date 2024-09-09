0d1afc1ef04e458798c57b50dc7251a37285e771
Sarah Abu irmeileh <211027@ppu.edu.ps>
2024-09-09 12:57:35 +0300

    Codeforces 1A

--- CodeForces/Rate 900/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
- if(("" + str[i] + str[i+1]+str[i+2]) == "WUB"){
-    //         i+=2;
-    //         if(i){
-    //             cout << " ";
-    //         }
-    //     }else{
-    //         cout << str[i];
-    //     }
-    // }
