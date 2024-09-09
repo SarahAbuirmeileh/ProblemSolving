3e109551546dc7b00a84c5905faa755196b2e1dd
Sarah Abu irmeileh <211027@ppu.edu.ps>
2024-09-09 13:56:41 +0300

    Codeforces 483A

---
+++ CodeForces/Rate 1100/483A.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n" 
+#define int long long
+#define tee { ios :: sync_with_stdio(false); cin.tie(0); cout.tie(0); }
+#define not_less_than(v, x) lower_bound(v.begin(), v. end(), x)                   // x >=
+#define not_more_than(v, x) lower_bound(v.rbegin(), v.rend(), x, greater<int>())  // x <=
+#define more_than(v, x) upper_bound (v.begin(), v.end(), x)                       // x <
+#define less_than(v, x)upper_bound (v. rbegin(), v.rend(), x, greater<int>())     // x >
+
+const int dx[] = { 0,0,1,-1 };
+const int dy[] = { 1,-1,0,0 };
+
+void solve();
+int32_t main() {
+    tee
+    // #ifndef ONLINE_JUDGE 
+    //     freopen("input.txt", "r", stdin); 
+    //     freopen("output.txt", "w", stdout); 
+    // #endif 
+    int t = 1;
+    // cin >> t;
+    for(int i = 1; i <= t; i++){
+        solve();
+    }
+    
+    return 0;
+}
+
+void solve(){
+
+    int l, r;
+    cin >> l >> r;
+
+    if(r-l <2){
+        cout << -1 << endl;
+    }else{
+        if(!(l%2)){
+            cout << l << " " << l+1  << " "<< l+2 << endl;
+        }else{
+            if(l+3 <= r){
+                cout << l+1 << " " << l+2 << " " << l+3 << endl;
+            }else{
+                cout << -1 << endl;
+            }
+        }
+    }
+}
