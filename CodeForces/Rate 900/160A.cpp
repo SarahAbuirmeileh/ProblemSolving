3e0c208fbf426a6b8afce238459e284881024f88
Sarah Abu irmeileh <211027@ppu.edu.ps>
2024-09-10 21:19:12 +0300

    Codeforces 160A

---
+++ CodeForces/Rate 900/160A.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n" 
+#define int long long
+#define tee { ios :: sync_with_stdio(false); cin.tie(0); cout.tie(0); }
+#define not_less_than(v, x) lower_bound(v.begin(), v. end(), x)                   // x >=
+#define not_more_than(v, x) lower_bound(v.rbegin(), v.rend(), x, greater<int>())  // x <=
+#define more_than(v, x) upper_bound (v.begin(), v.end(), x)                       // x <
+#define less_than(v, x)upper_bound (v. rbegin(), v.rend(), x, greater<int>())     // x >
+
+const int dx[] = { 0,0,1,-1 };
+const int dy[] = { 1,-1,0,0 };
+
+void solve();
+int32_t main() {
+    tee
+    // #ifndef ONLINE_JUDGE 
+    //     freopen("input.txt", "r", stdin); 
+    //     freopen("output.txt", "w", stdout); 
+    // #endif 
+    int t = 1;
+    // cin >> t;
+    for(int i = 1; i <= t; i++){
+        solve();
+    }
+    
+    return 0;
+}
+
+void solve(){
+
+    int n; cin >> n;
+    vector<int>v(n);
+
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> v[i]; 
+        sum += v[i];
+    }
+    sort(v.rbegin(), v.rend());
+    
+    int cur_sum = 0, counter=0;
+    for(int i=0; i<n; i++){
+        cur_sum+=v[i];
+        counter++;
+        if(cur_sum > sum-cur_sum){
+            cout << counter << endl;
+            return;
+        }
+    }
+}
