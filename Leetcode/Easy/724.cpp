dd37083fd3d6ae157aa068d33188f5db44c9dd7c
Sarah Abu irmeileh <211027@ppu.edu.ps>
2024-09-24 19:19:16 +0300

    Leetcode 724

---
+++ Leetcode/Easy/724.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int size = nums.size() + 1 ;
+        vector<int>sum (size );
+        for(int i = 1; i < size; i++){
+            sum[i] = sum[i - 1] + nums[i-1]; 
+            cout << sum[i] << " ";
+        }
+
+        for(int i = 1; i < size; i++){
+            if(sum[i - 1] == (sum[size - 1 ] - sum[i])){
+                return i - 1;
+            }
+        }
+        return -1;
+    }
+};
